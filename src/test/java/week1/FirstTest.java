2741204f96a1a2ddeb8404a5a32f7c985106cc04
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-12-13 21:15:40 +0300

    project created.

--- src/test/java/week1/FirstTest.java
+++ src/test/java/week1/FirstTest.java
@@ -1,4 +1,20 @@
 package week1;
 
+import io.restassured.*;
+import io.restassured.http.*;
+import io.restassured.response.*;
+import org.junit.jupiter.api.*;
+
 public class FirstTest {
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get("http://54.152.219.47:8000/api/spartans");
+
+        int statusCode = response.statusCode();
+        System.out.println("statusCode = " + statusCode);
+        response.prettyPrint();
+    }
 }
